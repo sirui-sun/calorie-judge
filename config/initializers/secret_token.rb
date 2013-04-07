bc736e69fdff6a64613eb2fb1440fb792ed33091
Sirui Sun <sirui.sun1@gmail.com>
2013-04-07 17:01:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalorieJudge::Application.config.secret_token = 'cfef4c0591940a103facecea7155267da6f991b29a5afc68c9645365fe55fae1885f9d3ed484624b79543bbd21d7a5437a1d1eca52f1189c7afd1ec567ce0788'
